ab37d94cc9d361df1ef1cd23455da270469311ec
Natali <117864746+Sofyhka@users.noreply.github.com>
2023-04-04 12:17:58 +0300

    Add files via upload

--- task1.java
+++ task1.java
@@ -1,16 +1,22 @@
-import java.util.Scanner;
+
+
+import java.util.Arrays;
 
 public class task1 {
-
     public static void main(String[] args) {
-        Scanner iScanner = new Scanner(System.in);
-        System.out.printf("Введите первое число: ");
-        int i = iScanner.nextInt();
-        System.out.printf("Треугольное число: %d\n", giveMeNumber(i));
-        iScanner.close();
-    }
-
-    public static int giveMeNumber(int a) {
-        return (a * (a + 1)) / 2;
+        int[] array = { 9, 4, 7, 3, 5, 7, 3, 5 }; // исходный массив
+        // Сортировка пузырьком
+        int temp; // переменная для замены
+        for (int i = array.length - 1; i > 0; i--) { // внешний цикл
+            for (int j = 0; j < i; j++) { // внутренний цикл
+                if (array[j] > array[j + 1]) { // сравнение
+                    temp = array[j]; // если истино, то меняем
+                    array[j] = array[j + 1]; // местами элементы
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        // конец сортировки
+        System.out.println(Arrays.toString(array)); // выводим результат на консоль
     }
 }
